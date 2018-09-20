a8f795021cec69841de113995ff2106cde5000a4
ira strikun <ira.strikun@gmail.com>
2018-09-20 21:56:27 +0300

    calculations almost finished

--- src/Homepage/Filters/Filters.jsx
+++ src/Homepage/Filters/Filters.jsx
@@ -7,11 +7,14 @@ import { data } from "../../db";
 class Filters extends Component {
   state = {
     value: 50000,
-    value2: 4,
-    percent: 0,
+    value2: 19,
+    percent: 18,
     currency: "USD",
     paymentType: "month",
-    summ: 0
+    summ: 750,
+    summAnual: 14250,
+    activeAnnual: true,
+    activeMonth: false
   };
 
   getPercent = () => {
@@ -31,13 +34,47 @@ class Filters extends Component {
 
     this.setState({ percent: percentNew }, () => {
       let summNew = ((this.state.percent / 100) * this.state.value) / 12;
+      let summAnnualNew = summNew * this.state.value2;
       this.setState(prevState => {
-        return { summ: summNew };
+        return {
+          summ: summNew,
+          summAnual: summAnnualNew
+        };
       });
     });
   };
 
+  changeTime(value2) {
+    this.setState({ value2: value2 });
+    this.getPercent();
+  }
+
+  changeAmount(value) {
+    this.setState({ value: value });
+    this.getPercent();
+  }
+
+  changePaymentType = type => {
+    if (type === "annual") {
+      this.setState({
+        paymentType: "annual",
+        activeAnnual: !this.state.activeAnnual,
+        activeMonth: !this.state.activeMonth
+      });
+      this.getPercent();
+    } else if (type === "month") {
+      this.setState({
+        paymentType: "month",
+        activeMonth: !this.state.activeMonth,
+        activeAnnual: !this.state.activeAnnual
+      });
+      this.getPercent();
+    }
+  };
+
   render() {
+    let btnClassAnnual = this.state.activeAnnual ? "ui-btn--active" : "ui-btn";
+    let btnClassMonth = this.state.activeMonth ? "ui-btn--active" : "ui-btn";
     return (
       <div className="filters-wrap">
         <div className="summ-wrap">
@@ -48,7 +85,8 @@ class Filters extends Component {
               maxValue={100000}
               minValue={1000}
               value={this.state.value}
-              onChange={value => this.setState({ value: value })}
+              // onChange={value => this.setState({ value: value })}
+              onChange={value => this.changeAmount(value)}
               formatLabel={value => `${value} $`}
             />
           </div>
@@ -61,8 +99,8 @@ class Filters extends Component {
               maxValue={36}
               minValue={3}
               value={this.state.value2}
-              onChange={value2 => this.setState({ value2: value2 })}
-              // onChange={() => this.change()}
+              // onChange={value2 => this.setState({ value2: value2 })}
+              onChange={value2 => this.changeTime(value2)}
               // onChange={this.onValueChange(value2)}
               formatLabel={value2 => `${value2} мес`}
             />
@@ -73,8 +111,19 @@ class Filters extends Component {
           {" "}
           <span className="filters__title">Выплата процентов</span>
           <div className="payments__btns-wrap">
-            <button className=" payments__btn ui-btn">ежимесячно</button>
-            <button className=" payments__btn ui-btn">в конце срока</button>
+            <button
+              onClick={() => this.changePaymentType("month")}
+              className={`${btnClassAnnual} payments__btn `}
+            >
+              ежимесячно
+            </button>
+            <button
+              onClick={() => this.changePaymentType("annual")}
+              // className=" payments__btn ui-btn"
+              className={`${btnClassMonth} payments__btn `}
+            >
+              в конце срока
+            </button>
           </div>
         </div>
 
@@ -90,7 +139,8 @@ class Filters extends Component {
         </div>
 
         <button onClick={() => this.getPercent()}>Get summ</button>
-        <p> {`${this.state.summ}$`}</p>
+        <p> {`${Math.ceil(this.state.summ)}$`}</p>
+        <p> {`${Math.ceil(this.state.summAnual)}$`}</p>
       </div>
     );
   }
